441f4220cc849cc24d28b4750f304ce426c525cd
seanpara <spara77@gmail.com>
2019-04-11 13:51:56 -0400

    added more css & html

--- index.js
+++
@@ -1,8 +0,0 @@
-const testButton = document.getElementById("test_button");
-
-if (testButton) {
-  testButton.addEventListener(
-    "click",
-    e => (e.target.style.backgroundColor = "red")
-  );
-}
